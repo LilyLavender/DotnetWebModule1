5b42ce680b59a1993cf9cd3f70cab75617c7fcfc
LilyLavender <lilylambda136@gmail.com>
2024-09-09 21:41:30 -0500

    [1.17] Assignment: Done

--- discount.js
+++ discount.js
@@ -3,7 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
       if (e.target.classList.contains('discount')) {
         e.preventDefault();
         document.getElementById('product').innerHTML = e.target.dataset['product'];
+        document.getElementById('discount-title').innerHTML = e.target.dataset['discountTitle'];
+        document.getElementsByClassName('toast-body')[0].innerHTML = "Discount Code: " + e.target.dataset['discountCode'];
         bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
       }
     });
   });
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "Escape") {
+        bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).hide();
+    }
+})
